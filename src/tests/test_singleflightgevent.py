bd5d69aa2b5df55af6294fbf7dbf8eb68b96eeca
aaron <aarondwico@gmail.com>
2020-05-25 19:27:31 +0700

    add gevent support

--- src/tests/test_singleflightgevent.py
+++ src/tests/test_singleflightgevent.py
@@ -1,15 +1,13 @@
 import unittest
-
-from time import sleep
-from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 
-from src.singleflight import SingleFlight
+from gevent import spawn, joinall, sleep
 
-class TestSingleFlight(unittest.TestCase):
+from src.singleflightgevent import SingleFlightGevent as SingleFlight
+
+class TestSingleFlightGevent(unittest.TestCase):
   def test_call_directly(self):
     sf = SingleFlight()
-    executor = ThreadPoolExecutor(max_workers=10)
 
     # success case
     counter = 0
@@ -22,15 +20,16 @@ class TestSingleFlight(unittest.TestCase):
 
     res = []
     for i in range(10):
-      sfc = partial(sf.call, work, "key", i+1)
-      r = executor.submit(sfc)
-      res.append(r)
+      p = partial(sf.call, work, "key", i+1)
+      g = spawn(p)
+      res.append(g)
+    joinall(res)
 
     for r in res:
-      self.assertEqual(r.result()[0],result)
+      self.assertEqual(r.value[0],result)
       # because only the first one can get the lock 
       # and only that one request call
-      self.assertEqual(r.result()[1],1) 
+      self.assertEqual(r.value[1],1)
       
     self.assertEqual(counter,1)
 
@@ -43,25 +42,25 @@ class TestSingleFlight(unittest.TestCase):
       counter_err += 1
       raise err
 
-    sfc_err = partial(sf.call, work_err, "key_err")
     res = []
     for i in range(10):
-      r = executor.submit(sfc_err)
-      res.append(r)
+      p = partial(sf.call, work_err, "key_err")
+      g = spawn(p)
+      res.append(g)
+    joinall(res)
 
     exception_count = 0
     for r in res:
-      self.assertRaises(NotImplementedError, r.result)
+      self.assertEqual(
+        NotImplementedError, 
+        type(r.exception))
       exception_count += 1
 
     self.assertEqual(exception_count, 10)
     self.assertEqual(counter_err, 1)
-  
-    executor.shutdown()
 
   def test_call_decorated(self):
     sf = SingleFlight()
-    executor = ThreadPoolExecutor(max_workers=10)
 
     # success case
     counter = 0
@@ -76,16 +75,16 @@ class TestSingleFlight(unittest.TestCase):
 
     res = []
     for i in range(10):
-      sfc = partial(work, "key", i+1)
-      r = executor.submit(sfc)
-      res.append(r)
+      p = partial(work, "key", i+1)
+      g = spawn(p)
+      res.append(g)
+    joinall(res)
 
     for r in res:
-      self.assertEqual(r.result()[0],result)
-
+      self.assertEqual(r.value[0],result)
       # because only the first one can get the lock 
       # and only that one request call
-      self.assertEqual(r.result()[1],1) 
+      self.assertEqual(r.value[1],1) 
       
     self.assertEqual(counter,1)
 
@@ -100,22 +99,23 @@ class TestSingleFlight(unittest.TestCase):
       counter_err += 1
       raise err
 
-    sfc_err = partial(work_err, "key_err")
     res = []
     for i in range(10):
-      r = executor.submit(sfc_err)
-      res.append(r)
+      p = partial(work_err, "key_err")
+      g = spawn(p)
+      res.append(g)
+    joinall(res)
 
     exception_count = 0
     for r in res:
-      self.assertRaises(NotImplementedError, r.result)
+      self.assertEqual(
+        NotImplementedError, 
+        type(r.exception))
       exception_count += 1
 
     self.assertEqual(exception_count, 10)
     self.assertEqual(counter_err, 1)
 
-    executor.shutdown()
-      
   def test_wrong_type_passed(self):
     sf = SingleFlight()
 
